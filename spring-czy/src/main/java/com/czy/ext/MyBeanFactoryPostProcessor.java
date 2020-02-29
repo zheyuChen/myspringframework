5ee28fb273809435164f88fe2b01ccf8cba54f0d
zheyuChen <1040206234@qq.com>
2020-02-29 21:37:22 +0800

    add

---
+++ spring-czy/src/main/java/com/czy/ext/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,17 @@
+package com.czy.ext;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        GenericBeanDefinition userService = (GenericBeanDefinition) beanFactory.getBeanDefinition("userService");
+        System.out.println("userService mode = " + userService.getAutowireMode());
+    }
+}
